d4579605510ef0b9b307356808a4b3088095335b
Susana Hahn <susuhahnml@yahoo.com.mx>
2020-07-31 11:46:55 +0200

    Plots with dahed lines

--- benchmarks/plot_benchamrks_script.py
+++ benchmarks/plot_benchamrks_script.py
@@ -9,15 +9,21 @@ import seaborn as sns
 N_OUT = 4
 
 def clean_df(df):
+    df.drop(df.columns[-1*N_OUT:], axis=1, inplace=True) #Drop min
+    df.drop(df.columns[-2*N_OUT:-1*N_OUT], axis=1, inplace=True) #Drop max
+    
     cols = df.columns[1:]
     cols = [c.split("/")[-1] for i,c in enumerate(cols) if i%N_OUT==0]
     params = df.iloc[0][1:N_OUT+1]
+
 
     new_cols = ["{}-{}".format(c,p) for c,p in list(itertools.product(cols, params))]
     new_cols = ["instance-name"] + new_cols
     df.drop(df.index[0], inplace=True)
 
+
     df.drop(df.tail(9).index,inplace=True)
+    # print(new_cols[-2*N_OUT]+new_cols[-1*N_OUT])
     df.columns = new_cols
     df.iloc[:,0]=df.iloc[:,0].apply(lambda x: "{}-{}".format(x.split('/')[0] , x[-5:-3]))
     for i in range(1,len(df.columns)):
@@ -28,7 +34,7 @@ def clean_df(df):
 # Make a data frame
 # approaches = ['asp1','asp2']
 # approaches = ['dfa','afw']
-approaches = ['dfa','afw','asp1','asp2']
+approaches = ['dfa','afw','asp2']
 approach_df = []
 for a in approaches:
     df = pd.read_csv('benchmarks/benchmarks-results/results_{}.csv'.format(a))
@@ -57,23 +63,29 @@ red = {15:'#F3AEAE',
     40:'#FF2D2D',
     80:'#CC0000'
     }
-    
+
+lines =   {15:'-',25:':',40:'-',80:':','all':'-'}
 # colors = [green,red]
 colors = [blue,yellow,green,red]
 
-out_values = ['time','choices','conflicts']
-# out_values = ['time']
+# out_values = ['time','choices','conflicts']
+out_values = ['time']
+all_labels_from1= approach_df[0][1]
+
+base_labels = set([s.split('_h_')[0] for s in all_labels_from1])
+nb_plots= len(base_labels)
 for out_value in out_values:
- 
-    # multiple line plot
+    
     num=0
+    # fig, axs = plt.subplots(nb_plots, sharex=True)
 
-    for i in range(0,len(approach_df[0][1])):
+    for i in range(0,len(all_labels_from1)):
     # for i in range(0,1):
-        plt.style.use('seaborn-dark-palette')
-        palette = plt.get_cmap('Set1')
-        column_base = "".join(approach_df[0][1][i].split('_h_')[0])
-
+        column_base = "".join(all_labels_from1[i].split('_h_')[0])
+        if not column_base in base_labels:
+            continue
+        else:
+            base_labels.remove(column_base)
         for ap_idx, (df,columns) in enumerate(approach_df):
             column_full_name=columns[i]
             approach_name = column_full_name.split('-')[-1]
@@ -84,15 +96,15 @@ for out_value in out_values:
                     h = int(column.split('_h_')[1].split('-')[0])
                 except:
                     plot_unsat = False
-                    h=40
+                    h='all'
                 num= num +1
 
+                label = "{}-{}".format(approach_name,h) if plot_unsat else approach_name
                 new_models = 1-df[column+'-models']
                 new_models[ new_models==0 ] = np.nan
                 only_on_unsat = new_models*df[column+'-{}'.format(out_value)]
-                label = "{}-{}".format(approach_name,h) if plot_unsat else approach_name
-                plt.plot(df['instance-name'], df[column+'-{}'.format(out_value)], marker='', linewidth=1, alpha=1, label=label, color=colors[ap_idx][h],zorder=-1)
-                if plot_unsat:
+                plt.plot(df['instance-name'], df[column+'-{}'.format(out_value)], lines[h],marker='', linewidth=1, alpha=1, label=label, color=colors[ap_idx][40],zorder=-1)
+                if plot_unsat and label!="median":
                     plt.scatter(df['instance-name'], only_on_unsat, color='r',s=1,alpha=1,zorder=1)
             # plt.tight_layout()
             # Add legend
