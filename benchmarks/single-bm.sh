48ad1709e0cce9b7a0af8ee8ecf6046a22dbd162
Susana Hahn <susuhahnml@yahoo.com.mx>
2020-08-03 17:21:42 +0200

    Single bm script

--- benchmarks/single-bm.sh
+++ benchmarks/single-bm.sh
@@ -1,6 +1,18 @@
-echo $PATH
-cd benchmark-tool
-./bgen runscripts/runscript_asprilo_nc.xml
-python2 asprilo_NO-CONSTARINT_benchmark/clingo-seq-job/komputer/start.py
-./beval runscripts/runscript_asprilo_no_constraint.xml >results/ benchmark_evaluated.xml
-./bconv -m time,models,choices,conflicts  results/benchmark_evaluated.xml > results/results_no_constraint.ods
+#!/bin/bash
+APPROACH=$1
+# BT_PATH=benchmark-tool
+BT_PATH=atlingo/benchmarks/benchmark-tool
+HORIZON=$2
+NEW_RUNSCRIPT=./runscripts/runscript_asprilo_${APPROACH}__h-$HORIZON.xml
+echo "Generate..."
+# cd $BT_PATH
+cd ~/temporal-automata/$BT_PATH
+sed 's/{H}/'$HORIZON'/g' ./runscripts/runscript_asprilo_$APPROACH.xml > $NEW_RUNSCRIPT
+rm -rf scrips_asprilo_${APPROACH}__h-$HORIZON
+./bgen $NEW_RUNSCRIPT
+echo "Start..."
+python2 asprilo_${APPROACH}_benchmark/clingo-seq-job/komputer/start.py
+echo "Eval..."
+./beval $NEW_RUNSCRIPT > results/benchmark_evaluated_${APPROACH}__h-$HORIZON.xml
+echo "Conv..."
+./bconv -m time,models,choices,conflicts  results/benchmark_evaluated__${APPROACH}__h-$HORIZON.xml > results/results_${APPROACH}__h-$HORIZON.ods
