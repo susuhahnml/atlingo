23a0b7b52c11119fcf998c7260e371ce8512e65c
Susana Hahn <susuhahnml@yahoo.com.mx>
2020-08-03 15:27:50 +0200

    cluster fix

--- benchmarks/cluster_script.sh
+++ benchmarks/cluster_script.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 #SBATCH --time=00:10:00 
-~/temporal-automata/atlingo/benchmarks
-cd /benchmark-tool/
+#SBATCH -N 1 # number of nodes
+#SBATCH -n 1 # number of cores
+#SBATCH --chdir= ~/temporal-automata/atlingo/benchmarks/benchmark-tool
+echo pwd
+echo "Holi"
 srun ./bgen runscripts/runscript_asprilo_dfa.xml
