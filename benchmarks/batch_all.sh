80aed9a7c3b49d0c157a0fc1a7891b0001439c35
Susana Hahn <susuhahnml@yahoo.com.mx>
2020-08-05 15:40:42 +0200

    Ourput changed

--- benchmarks/batch_all.sh
+++ benchmarks/batch_all.sh
@@ -1,30 +1,14 @@
 #!/bin/sh
-N=1
-APP=no-constraint
-HORIZON=15
-sed 's/{H}/'$HORIZON'/g; s/{N}/'$N'/g; s/{APP}/'$APP'/g' ./cluster_script.sh > ./cluster_script__${APP}__${HORIZON}__${N}.sh
-echo sbatch cluster_script__${APP}__${HORIZON}__${N}.sh
-HORIZON=25
-sed 's/{H}/'$HORIZON'/g; s/{N}/'$N'/g; s/{APP}/'$APP'/g' ./cluster_script.sh > ./cluster_script__${APP}__${HORIZON}__${N}.sh
-echo sbatch cluster_script__${APP}__${HORIZON}__${N}.sh
-HORIZON=35
-sed 's/{H}/'$HORIZON'/g; s/{N}/'$N'/g; s/{APP}/'$APP'/g' ./cluster_script.sh > ./cluster_script__${APP}__${HORIZON}__${N}.sh
-echo sbatch cluster_script__${APP}__${HORIZON}__${N}.sh
-HORIZON=45
-sed 's/{H}/'$HORIZON'/g; s/{N}/'$N'/g; s/{APP}/'$APP'/g' ./cluster_script.sh > ./cluster_script__${APP}__${HORIZON}__${N}.sh
-echo sbatch cluster_script__${APP}__${HORIZON}__${N}.sh
+./generate_run_script.sh no-constraint 15 1
+./generate_run_script.sh no-constraint 25 1
+./generate_run_script.sh no-constraint 35 1
+./generate_run_script.sh no-constraint 45 1
 
+./generate_run_script.sh afw 15 1
+./generate_run_script.sh afw 25 1
+./generate_run_script.sh afw 35 1
+./generate_run_script.sh afw 45 1
 
-APP=afw
-HORIZON=15
-sed 's/{H}/'$HORIZON'/g; s/{N}/'$N'/g; s/{APP}/'$APP'/g' ./cluster_script.sh > ./cluster_script__${APP}__${HORIZON}__${N}.sh
-echo sbatch cluster_script__${APP}__${HORIZON}__${N}.sh
-HORIZON=25
-sed 's/{H}/'$HORIZON'/g; s/{N}/'$N'/g; s/{APP}/'$APP'/g' ./cluster_script.sh > ./cluster_script__${APP}__${HORIZON}__${N}.sh
-echo sbatch cluster_script__${APP}__${HORIZON}__${N}.sh
-HORIZON=35
-sed 's/{H}/'$HORIZON'/g; s/{N}/'$N'/g; s/{APP}/'$APP'/g' ./cluster_script.sh > ./cluster_script__${APP}__${HORIZON}__${N}.sh
-echo sbatch cluster_script__${APP}__${HORIZON}__${N}.sh
-HORIZON=45
-sed 's/{H}/'$HORIZON'/g; s/{N}/'$N'/g; s/{APP}/'$APP'/g' ./cluster_script.sh > ./cluster_script__${APP}__${HORIZON}__${N}.sh
-echo sbatch cluster_script__${APP}__${HORIZON}__${N}.sh
+./generate_run_script.sh no-constraint 15 0
+
+./generate_run_script.sh afw 15 0
--- benchmarks/cluster_script.sh
+++ benchmarks/cluster_script.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 #SBATCH --time=00:59:00 
 #SBATCH -N 1 # number of nodes
-#SBATCH --output=out.%j
-#SBATCH --error=err.%j
+#SBATCH --output=slrum_out/out.{APP}-{H}-{N}
+#SBATCH --error=slrum_out/out.{APP}-{H}-{N}
 #SBATCH --job-name={APP}-{H}-{N}
 $PATH_AWF = atlingo/benchmarks/benchmark-tool
 set -e
