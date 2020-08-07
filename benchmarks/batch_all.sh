50f933d4d3e03d0dee3d08fdf60efd4839014902
Susana Hahn Martin Lunas <hahnmartinlu@zuse.cs.uni-potsdam.de>
2020-08-07 12:57:56 +0200

    Results from first full run

--- benchmarks/batch_all.sh
+++ benchmarks/batch_all.sh
@@ -1,17 +1,17 @@
 #!/bin/sh
-./run_bm.sh afw 15 1
-./run_bm.sh afw 25 1
-./run_bm.sh afw 35 1
+#./run_bm.sh afw 15 1
+#./run_bm.sh afw 25 1
+#./run_bm.sh afw 35 1
 #./run_bm.sh afw 45 1
 
-./run_bm.sh nc 15 1
-./run_bm.sh nc 25 1
-./run_bm.sh nc 35 1
+#./run_bm.sh nc 15 1
+#./run_bm.sh nc 25 1
+#./run_bm.sh nc 35 1
 #./run_bm.sh nc 45 1
 
-./run_bm.sh asp 15 1
-./run_bm.sh asp 25 1
-./run_bm.sh asp 35 1
+#./run_bm.sh asp 15 1
+#./run_bm.sh asp 25 1
+#./run_bm.sh asp 35 1
 #./run_bm.sh asp 45 1
 
 ./run_bm.sh dfa 15 1
