29dd7536e919e9e6040f4c95092a528309485fe9
Susana Hahn <susuhahnml@yahoo.com.mx>
2020-08-31 18:45:34 +0200

    Global extra files. Corrected tuple bug. Clean benchamrks

--- benchmarks/plot_script.sh
+++ benchmarks/plot_script.sh
@@ -4,3 +4,11 @@
 python plot.py --approach afw  --approach afw_del --approach asp --approach nc --prefix="choices-"  --horizon 6 --models 0 --table --stat choices --mean
 
 python plot.py --approach afw  --approach afw_del --approach asp --approach nc --prefix="conflicts-"  --horizon 6 --models 0 --table --stat conflicts --mean
+
+
+python plot.py --approach proj-afw  --approach proj-afw_del --approach proj-asp --prefix="proj-models"  --horizon 7 --models 0 --table --stat models --mean --table
+
+
+
+python plot.py --approach afw --approach asp --approach afw_del --prefix="time-g-models-"  --horizon 6  --stat ctime --stat csolve --models 0 --group
+python plot.py --approach afw --approach asp --approach afw_del --prefix="time-g-models-"  --horizon 7  --stat ctime --stat csolve --models 0 --group
