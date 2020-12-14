4c6a6d06dd79044c0d7df547231a7d42197b8135
Susana Hahn <susuhahnml@yahoo.com.mx>
2020-12-13 22:00:11 -0600

    First remove of test

--- scripts/viz.py
+++ scripts/viz.py
@@ -6,6 +6,8 @@ import sys
 import networkx as nx
 import pygraphviz as pgv        
 import clingo
+import itertools
+import pdb
 class Context:
     def id(self, x):
         return x
@@ -107,6 +109,72 @@ def str_formula(symbol,maps):
     return s
 
 
+def add_node(g, d):
+    #Define shape
+    if d['type']=='state':    
+        shape = 'ellipse'
+    if d['type']=='trans':    
+        shape = 'square'
+    if d['type']=='test':    
+        shape = 'triangle'
+    if d['type']=='bool':    
+        shape = 'plain'
+    #Define color an create node
+    if d['val']=='true':
+        g.add_node(d['id'],shape=shape,label=d['val'],fontcolor='#7BAA16',type=d['type'])
+    elif d['val']=='false':
+        g.add_node(d['id'],shape=shape,label=d['val'],fontcolor='#AA1616',type=d['type'])
+    elif d['type']=='state':
+        g.add_node(d['id'],shape=shape,label=d['val'],fillcolor='#00305E20',style='filled',type=d['type'])
+    elif d['type']=='trans':
+        g.add_node(d['id'],shape=shape,label=d['val'],type=d['type'],fillcolor='#FFFFFF')
+    elif d['type']=='test':
+        g.add_node(d['id'],shape=shape,label=d['val'],type=d['type'],fillcolor='#FFFFFF')
+    else:
+        raise RuntimeError("Invalid type "+d['type'])
+
+
+
+def remove_test(g_old, g_new, n_from_id, n_to_rem, pi, prop_used, counters,l):
+        print(counters)
+        print(l)
+        # G_min.draw('{}/img_a_{}_min-{}.png'.format(base_path,i,n),prog='dot')
+        # pdb.set_trace()
+        if(n_to_rem.attr['type']=='bool'):
+            print('Bool')
+            add_node(g_new, {'type':'bool','val':n_to_rem.attr['label'],'next':[],'id':counters['next_id']})
+            g_new.add_edge(n_from_id,counters['next_id'],label=l)
+            counters['next_id']+=1
+        elif(n_to_rem.attr['type']=='state'):
+            g_new.add_edge(n_from_id,n_to_rem,label=l)
+        elif(n_to_rem.attr['type']=='trans'):
+            print(n_to_rem.attr['label'])
+            current_id=counters['next_id']
+            add_node(g_new, {'type':'trans','val':n_to_rem.attr['label'],'next':[],'id':counters['next_id']})
+            n_l = g_old.out_neighbors(n_to_rem)[0]
+            n_r = g_old.out_neighbors(n_to_rem)[1]
+            g_new.add_edge(n_from_id,current_id,label=l)
+            counters['next_id']+=1
+            remove_test(g_old,g_new,current_id,n_l,pi,prop_used,counters,"")
+            remove_test(g_old,g_new,current_id,n_r,pi,prop_used,counters,"")
+        elif(n_to_rem.attr['type']=='test'):
+            e_l = g_old.out_edges(n_to_rem)[0]
+            id_l = int(e_l.attr['prop_id'])
+            e_r = g_old.out_edges(n_to_rem)[1]
+            id_r = int(e_r.attr['prop_id'])
+            id_prop = abs(id_l)
+            not_edge = e_l if id_l<id_r else e_r
+            in_edge = e_l if id_l>id_r else e_r
+            not_path = id_prop in pi
+            if not_path:
+                chossen_e = not_edge
+            else:
+                chossen_e = in_edge
+            prop_used.add(int(chossen_e.attr['prop_id']))
+            remove_test(g_old,g_new,n_from_id,chossen_e[1],pi,prop_used,counters,l)
+        else:
+            raise RuntimeError("Invalid type")
+
 # Load automaton facts
 
 ctl = clingo.Control("0")
@@ -119,56 +187,34 @@ with ctl.solve(yield_=True) as handle:
         maps = {str(a.arguments[0].number):construct_str_map(a.arguments[1]) for a in atoms if a.name=='id_map'}
         delta = {str_formula(a.arguments[0],maps):a.arguments[1] for a in atoms if a.name=='delta'}
         initials = [str_formula(a.arguments[0],maps) for a in atoms if a.name=='initial_state']
-
-
+        prop_ids = [a.arguments[0].number for a in atoms if a.name=='prop_id' or a.name=='last_id']
 # Create automaton
 for i,initial in enumerate(initials):
     G=pgv.AGraph(ranksep='0.5',directed=True)
-    pending_nodes = [{'type':'state','val':initial,'next':[("",delta[initial])],'id':0}]
-    next_id = 0
+    pending_nodes = [{'type':'state','val':initial,'next':[(0,"",delta[initial])],'id':0}]
+    counters = {'next_id': 0}
     idx = 0
     while idx< len(pending_nodes):
         d=pending_nodes[idx]
-        
-        #Define shape
-        if d['type']=='state':    
-            shape = 'ellipse'
-        if d['type']=='trans':    
-            shape = 'square'
-        if d['type']=='test':    
-            shape = 'triangle'
-        if d['type']=='bool':    
-            shape = 'plain'
-        # next_str = "*".join(["*".join([a[0],str(a[1].arguments[0])]) for a in d['next']])
-        #Define color
-        if d['val']=='true':
-            G.add_node(d['id'],shape=shape,label=d['val'],fontcolor='#7BAA16',type=d['type'])
-        elif d['val']=='false':
-            G.add_node(d['id'],shape=shape,label=d['val'],fontcolor='#AA1616',type=d['type'])
-        elif d['type']=='state':
-            G.add_node(d['id'],shape=shape,label=d['val'],fillcolor='#00305E20',style='filled',type=d['type'])
-        else:
-            G.add_node(d['id'],shape=shape,label=d['val'],type=d['type'])
-
-
-
-        for l,f in d['next']:
-            next_id+=1
+        add_node(G,d)
+        for n,l,f in d['next']:
+            counters['next_id']+=1
             if (f.name=='pbf_true'):
-                next_node = {'type':'bool','val':'true','next':[],'id':next_id}
+                next_node = {'type':'bool','val':'true','next':[],'id':counters['next_id']}
             elif (f.name=='pbf_false'):
-                next_node = {'type':'bool','val':'false','next':[],'id':next_id}
+                next_node = {'type':'bool','val':'false','next':[],'id':counters['next_id']}
             elif (f.name=='pbf_or'):
-                next_node = {'type':'trans','val':'OR','next':[("",f.arguments[0]),("",f.arguments[1])],'id':next_id}
+                next_node = {'type':'trans','val':'OR','next':[(0,"",f.arguments[0]),(0,"",f.arguments[1])],'id':counters['next_id']}
             elif (f.name=='pbf_and'):
-                next_node = {'type':'trans','val':'AND','next':[("",f.arguments[0]),("",f.arguments[1])],'id':next_id}
+                next_node = {'type':'trans','val':'AND','next':[(0,"",f.arguments[0]),(0,"",f.arguments[1])],'id':counters['next_id']}
             elif (f.name=='pbf_if'):
-                next_node = {'type':'test','val':'?','next':[(maps[str(f.arguments[0])],f.arguments[1]),("~"+maps[str(f.arguments[0])],f.arguments[2])],'id':next_id}
+                next_node = {'type':'test','val':'?','next':[(int(f.arguments[0].number),maps[str(f.arguments[0])],f.arguments[1]),(-1*int(f.arguments[0].number),"~"+maps[str(f.arguments[0])],f.arguments[2])],'id':counters['next_id']}
+                prop_id = str(f.arguments[0])
             elif (f.name=='pbf_state'):    
                 id_state = str_formula(f.arguments[0],maps)
-                next_node = {'type':'state','val':id_state,'next':[("",delta[id_state])],'id':next_id}
+                next_node = {'type':'state','val':id_state,'next':[(0,"",delta[id_state])],'id':counters['next_id']}
             
-            connect_id = next_id
+            connect_id = counters['next_id']
             existent= False
             if next_node['type']=='state':
                 found = [p['id'] for p in pending_nodes if p['val']==next_node['val']]
@@ -176,109 +222,46 @@ for i,initial in enumerate(initials):
                     connect_id = found[0]
                     existent=True
 
-            G.add_edge(d['id'],connect_id,label=l)
+            G.add_edge(d['id'],connect_id,label=l,splines='curved',prop_id=n)
             if not existent: pending_nodes.append(next_node)
+        
         idx+=1            
 
     # Print automaton
     name = '{}/img_a_{}.png'.format(base_path,i)
-    # ['ghandle', 'attr', 'handle', 'encoding', '__module__', '__doc__', '__new__', 'get_handle', 'get_name', 'name', '__dict__', '__weakref__', '__repr__', '__hash__', '__str__', '__getattribute__', '__lt__', '__le__', '__eq__', '__ne__', '__gt__', '__ge__', '__iter__', '__mod__', '__rmod__', '__len__', '__getitem__', '__add__', '__mul__', '__rmul__', '__contains__', 'encode', 'replace', 'split', 'rsplit', 'join', 'capitalize', 'casefold', 'title', 'center', 'count', 'expandtabs', 'find', 'partition', 'index', 'ljust', 'lower', 'lstrip', 'rfind', 'rindex', 'rjust', 'rstrip', 'rpartition', 'splitlines', 'strip', 'swapcase', 'translate', 'upper', 'startswith', 'endswith', 'isascii', 'islower', 'isupper', 'istitle', 'isspace', 'isdecimal', 'isdigit', 'isnumeric', 'isalpha', 'isalnum', 'isidentifier', 'isprintable', 'zfill', 'format', 'format_map', '__format__', 'maketrans', '__sizeof__', '__getnewargs__', '__setattr__', '__delattr__', '__init__', '__reduce_ex__', '__reduce__', '__subclasshook__', '__init_subclass__', '__dir__', '__class__']
+
+
+    
+
+
     edges = G.edges()
     
     G_min=pgv.AGraph(ranksep='0.5',directed=True)
     name_min = '{}/img_a_{}_min.png'.format(base_path,i)
     state_nodes = [n for n in G.nodes() if n.attr['type']=='state']
+    pis = []
+    for i in range(len(prop_ids)+1):
+        pis+=([list(l) for l in itertools.combinations(prop_ids, i)])
+    
+    max_id = 0
     for n in state_nodes:
-        G_min.add_node(n,shape='ellipse',label=n.attr['label'],fillcolor='#00305E20',style='filled',type='state',val=n.attr['label'])
+        max_id = max_id if int(n)<max_id else int(n)
+        add_node(G_min,{'id':n,'val':n.attr['label'],'type':'state'})
+    
+
+    counters = {'next_id':max_id+1}
     for n in state_nodes:
-        print("\n\nNode!!!!!")
-        # labels, next{type:((state, bool, and), value:}
         
-        next_node_id = G.out_neighbors(n)[0]
-        next_node = G.get_node(next_node_id)
-        initial_edge = {'labels':[],'next':{'type':None,'val':None}}
-        # initial_edge['from']=n
-        all_edges = [initial_edge]
-        first_pending = {
-            "edge": initial_edge,
-            "next": initial_edge['next'],
-            "next_node_id": next_node_id
-        }
-        pending = [first_pending]
-        idx = 0
-        while idx< len(pending):
-            print("---- Pending ---- ")
-            p=pending[idx]
-            print(p)
+        print("Node: \n id:{}, label:{}\n".format(n,n.attr['label']))
+        n_next = G.out_neighbors(n)[0]
+        for p_idx, pi in enumerate(pis):
+            print("For pi={}".format(pi))
+            prop_used = set()
+            remove_test(G,G_min,n,n_next,pi,prop_used,counters,"{{{}}}".format(",".join([maps[str(p)] for p in pi])))
+            G_min.out_edges(n)[p_idx].attr['label']=prop_used
+            # g_new.add_edge(n,counters['next_id'],label=l)
 
-            idx=idx+1
-            next_node = G.get_node(p['next_node_id'])
-            print("type:" + next_node.attr['type'])
-            print("label:" + next_node.attr['label'])
-            print("")
-            out_edges=G.out_edges(next_node)
-            out_neighbors=G.out_neighbors(next_node)
-            if(next_node.attr['type']=='bool'):
-                p['next']['type']='bool'
-                p['next']['val']=next_node.attr['label']
-            elif(next_node.attr['type']=='state'):
-                p['next']['type']='state'
-                p['next']['val']=next_node.attr['label']
-            elif(next_node.attr['type']=='trans'):
-                if(next_node.attr['label']=='OR'):
-                    left = {"edge":copy.deepcopy(p['edge']),"next":p["next"]}
-                    left['next_node_id']= out_neighbors[0]
-                    all_edges.append(left['edge'])
-                    pending.append(left)
 
-                    right = {"edge":p['edge'],"next":p["next"]}
-                    right['next_node_id']= out_neighbors[1]
-                    pending.append(right)
-                else:
-                    p['next']['type']='and'
-                    left_next = {'type':None,'val':None}
-                    left_next_node = G.get_node(out_neighbors[0])
-                    pending.append({'edge':p['edge'],'next':left_next,'next_node_id':out_neighbors[0]})
-
-                    right_next = {'type':None,'val':None}
-                    right_next_node = G.get_node(out_neighbors[1])
-                    pending.append({'edge':p['edge'],'next':right_next,'next_node_id':out_neighbors[1]})
-
-                    p['next']['val']=(left_next,right_next)
-
-            elif(next_node.attr['type']=='test'):
-                left = {"edge":copy.deepcopy(p['edge']),"next":p["next"]}
-                left['next_node_id']= out_neighbors[0]
-                all_edges.append(left['edge'])
-                left['edge']['labels'].append(out_edges[0].attr['label'])
-                pending.append(left)
-
-                right = {"edge":p['edge'],"next":p["next"]}
-                right['next_node_id']= out_neighbors[1]
-                right['edge']['labels'].append(out_edges[1].attr['label'])
-                pending.append(right)
-
-            else:
-                raise RuntimeError("Invalid type "+next_node.attr['type'])
-            
-            print("Pending visited")
-            for e in pending[:idx]:
-                print(e)
-            print("Still pending:")
-            for e in pending[idx:]:
-                print(e)
-            print("")
-
-            print("all edges:")
-            for e in all_edges:
-                print(e)
-            print("")
-        for e in all_edges:
-            print(e)
-            # G.add_edge(n,connect_id,label=" ".(e.labels))
- 
-        # print(n.__dir__())
-    # print(G.string())
     G_min.draw(name_min,prog='dot')  
     G.draw(name,prog='dot')  
     print("Automaton {} saved in {}".format(i,name))
